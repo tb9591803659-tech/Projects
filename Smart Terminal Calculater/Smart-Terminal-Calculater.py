44af89b4e7c5b76c684feabeb99c8dd9b9c1cdcb
Shashank T <tb9591803659@gmail.com>
2026-05-29 19:23:31 +0530

    added error handlign and some terminal appealence using coloroma library

--- Smart Terminal Calculater/Smart-Terminal-Calculater.py
+++ Smart Terminal Calculater/Smart-Terminal-Calculater.py
@@ -1,5 +1,8 @@
 #Smart calculater interface as terminal runs in loop
 import os
+from colorama import Fore, Back, Style, init
+
+init(autoreset=True)
 
 def add(a , b):
     return a + b
@@ -21,50 +24,57 @@ def history_add(expression) :
 #Now a terminal interface
 print("CALCULATER\n")
 while True :
-   print("1.Calculate\n2.View History\n3.Exit\n4.Delete History")
-   option = int(input("Enter any option: "))
-   if option == 1:
-      exp = str(input("Enter Expression: "))
+   print(Fore.YELLOW + "[1]" + "Calculate") #\n2.View History\n3.Exit\n4.Delete History")
+   print(Fore.YELLOW + "[2]" + "View History")
+   print(Fore.YELLOW + "[3]" + "Exit")
+   print(Fore.YELLOW + "[4]" + "Delete History")
+   try:
+      option = int(input(Fore.BLUE  + Style.BRIGHT + "Enter any option: "))
+      if option == 1:
+         exp = input(Fore.BLUE  + Style.BRIGHT + "Enter Expression: ")
 
-      if "+" in exp :
-         parts = exp.split("+")
-         result = add(float(parts[0]),float(parts[1]))
+         if "+" in exp :
+            parts = exp.split("+")
+            result = add(float(parts[0]),float(parts[1]))
 
-      elif "-" in exp:
-         parts = exp.split("-")
-         result = sub(float(parts[0]),float(parts[1]))
+         elif "-" in exp:
+            parts = exp.split("-")
+            result = sub(float(parts[0]),float(parts[1]))
 
-      elif "*" in exp :
-         parts = exp.split("*")
-         result = mul(float(parts[0]),float(parts[1]))
+         elif "*" in exp :
+            parts = exp.split("*")
+            result = mul(float(parts[0]),float(parts[1]))
 
-      elif "/" in exp:
-         parts = exp.split("/")
-         if float(parts[1]) == 0 :
-            print("Error: not able to divide by zero")
+         elif "/" in exp:
+            parts = exp.split("/")
+            if float(parts[1]) == 0 :
+               print("Error: not able to divide by zero")
+               continue
+            result = div(float(parts[0]),float(parts[1]))
+         
+         else:
+            print("Invalid Expression!\nTry Again...")
             continue
-         result = div(float(parts[0]),float(parts[1]))
-      
-      else:
-         print("Invalid Expression!\nTry Again...")
-         continue
-      history_add(exp)
-      print("Final Result = ",result)
-   
-   elif option == 2:
-      if os.path.exists("history.txt") : 
-         with open("history.txt","r") as file: 
-            for line in file : 
-               print(line.strip())
+         history_add(exp)
+         print(Fore.GREEN  + f"Final Result = {result}")
+
+      elif option == 2:
+         if os.path.exists("history.txt") : 
+            with open("history.txt","r") as file: 
+               for line in file : 
+                  print(Fore.WHITE + Style.BRIGHT + line.strip() + Style.RESET_ALL)
+         else :
+            print()
+
+      elif option == 3:
+         print(Fore.GREEN + "Exiting Calculater...\nExited Calculater.")
+         break
+
+      elif option == 4:
+         pass #Has to be done and also time stamp should be added and then scientific calculation parsing system should be added
+
       else :
-         print()
-
-   elif option == 3:
-      print("Exiting Calculater...\nExited Calculater.")
-      break
-
-   elif option == 4:
-      pass #Has to be done and also time stamp should be added and then scientific calculation parsing system should be added
-
-   else :
-      print("Invalid option...\nTry again.")
+         print(Fore.RED + "Invalid option...\nTry again.")
+      
+   except Exception as e :
+      print(e)
