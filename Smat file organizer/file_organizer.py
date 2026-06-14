7c8ccc5bcc6e8d04f26c89a6a10d62488e41c7a5
Shashank T <tb9591803659@gmail.com>
2026-06-14 07:56:19 +0530

    Added basic classifier ahve to use shutil and move them later

--- Smat file organizer/file_organizer.py
+++ Smat file organizer/file_organizer.py
@@ -1,6 +1,21 @@
+
 import os 
 
+def file_detector(extension) :
+    if extension in [".jpg",".png"] :
+        print("Image")
+    elif extension in ["mp4"] :
+        print("Video")
+    elif extension in ["mp3"] :
+        print("Audio")
+    else :
+        print("Other document")
+    
 files = os.listdir()
 
 for file in files :
-    print(file)
+    name, extension = os.path.splitext(file)
+    print("Filename =",name," Extension =",extension)
+    file_detector(extension)
+    
+
