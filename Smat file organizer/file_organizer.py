7f799eef624f029ed8b9b1eb5adafaaa63d638a1
Shashank T <tb9591803659@gmail.com>
2026-06-14 09:48:08 +0530

    created basic level of organizer next step is to make it vaible for all type of files and extensions

--- Smat file organizer/file_organizer.py
+++ Smat file organizer/file_organizer.py
@@ -1,21 +1,45 @@
 
 import os 
+import shutil 
 
 def file_detector(extension) :
     if extension in [".jpg",".png"] :
-        print("Image")
-    elif extension in ["mp4"] :
-        print("Video")
-    elif extension in ["mp3"] :
-        print("Audio")
+        
+        return "Image"
+    elif extension in [".mp4"] :
+        
+        return "Video"
+    elif extension in [".mp3"] :
+        
+        return "Audio"
     else :
-        print("Other document")
+        
+        return "Other"
     
-files = os.listdir()
+def file_mover(file,directory) :
+    name, extension = os.path.splitext(file)
+    category = file_detector(extension)
+    source = os.path.join(directory,file)
+    if not os.path.isfile(source):
+        return
+    destination_folder = os.path.join(directory,category)
+    os.makedirs(destination_folder,exist_ok=True)
+    destination = os.path.join(destination_folder,file)
+    print("From:",source)
+    print("To:",destination)
+    shutil.move(source,destination)
+    print(f"Moved{file}->{category}")
+
+directory = input("Enter the path: ")
+
+files = os.listdir(directory)
 
 for file in files :
     name, extension = os.path.splitext(file)
-    print("Filename =",name," Extension =",extension)
-    file_detector(extension)
+    category = file_detector(extension)
+    print(file , " -> ", category)
+    
+    file_mover(file,directory)
+
     
 
